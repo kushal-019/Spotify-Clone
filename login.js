ba4460f075729c50d2300bdc74f39bac79934d9c
kushal goel <kgoel8451@gmail.com>
2024-02-16 16:55:05 +0530

    created spotify clone

---
+++ login.js
@@ -0,0 +1,39 @@
+
+let warn_msg = document.getElementsByClassName('warning');
+function show_warning(){
+    warn_msg[0].style.display = 'flex';
+};
+
+
+let pass_show = document.getElementsByClassName('pass_show')[0];
+let pass = document.getElementById('pass');
+
+function type_change(){
+    if(pass_show.classList.contains('bi-eye-fill')){
+        pass_show.classList.add('bi-eye-slash-fill');
+        pass_show.classList.remove('bi-eye-fill');
+        pass.type = 'password';
+    }
+    else{
+        pass_show.classList.remove('bi-eye-slash-fill');
+        pass_show.classList.add('bi-eye-fill');
+        pass.type = 'text';
+    }
+};
+
+let email = document.getElementById('email');
+let detail = document.getElementById('wrong_detail');
+function form_submit(){
+    if(email.value==''){
+        warn_msg[1].style.display = 'flex';
+        event.preventDefault();
+    }
+    else{
+        event.preventDefault();
+        detail.style.display ='block';
+    }
+    if(email.value == 'kushal1835.be22@chitkara.edu.in' && pass.value == '123456'){
+        window.close('login.html');
+    }
+};
+
---
+++ signup.js
@@ -0,0 +1,30 @@
+
+let email = document.querySelector('#email');
+let warning = document.querySelectorAll('.warn')[0];
+let submit_btn = document.querySelectorAll('.submit')[0];
+
+submit_btn.addEventListener('click', function(e){
+    if((email.value === "kushal.1835.be22@chitkara.edu.in" && email.type==='email' )||( email.value === '7056059847'&& email.type==='text' )){
+        window.close('sign_up.html');
+    }
+    else{
+        warning.style.display = 'block';
+        event.preventDefault();
+    }
+});
+
+let phn_no = document.getElementsByClassName('phn')[0];
+let lebel = document.getElementById('label');
+let  example= document.getElementsByClassName('example');
+
+phn_no.addEventListener('click' , function(e){
+    label.innerHTML = label.innerHTML==='Email Address'?'Mobile Number':'Email Address';
+    email.type =email.type==='text'?'email':'text';
+    email.placeholder = email.placeholder==="98555*****"?"name@domain.com":"98555*****";
+    phn_no.innerHTML = phn_no.innerHTML==="Use Email instead"?"Use phone number instead":"Use Email instead";
+    example[1].innerHTML = example[1].innerHTML==='example@email.com'? '0123456789':'example@email.com';
+    example[0].innerHTML = example[0].innerHTML==='email'?'mobile number':'email';
+    email.value ='';
+
+    event.preventDefault();
+});
